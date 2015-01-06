57f2915bfdcfabbfac5ca10f356bbf8cf7f967fe
Christoph Wille <christoph.wille@gmail.com>
2015-01-06 14:12:28 +0100

    Use Font Awesome for folder/file icons

--- Source/WpaGhApp/DesignViewModels/RepositoryContentsViewModelDT.cs
+++ Source/WpaGhApp/DesignViewModels/RepositoryContentsViewModelDT.cs
@@ -24,7 +24,14 @@ namespace WpaGhApp.DesignViewModels
                     RelativePath = "",
                     Size = 45,
                     ItemType = Octokit.TreeType.Blob
-                }
+                },
+                new GhTreeItem()
+                {
+                    Name = "Source",
+                    RelativePath = "",
+                    Size = 0,
+                    ItemType = Octokit.TreeType.Tree
+                },
             };
 
             Breadcrumbs = new ObservableCollection<GhTreeItem>(new List<GhTreeItem>()
--- Source/WpaGhApp/Models/GhTreeItem.cs
+++ Source/WpaGhApp/Models/GhTreeItem.cs
@@ -4,6 +4,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Octokit;
 
 namespace WpaGhApp.Models
 {
@@ -29,6 +30,26 @@ namespace WpaGhApp.Models
         public string Name { get; set; }
         public string RelativePath { get; set; }
 
+        public string ItemTypeAsFontawesomeGlyph
+        {
+            get
+            {
+                switch (ItemType)
+                {
+                    case TreeType.Blob:
+                        return "\uf016"; // fa-file-o
+                        break;
+                    case TreeType.Tree:
+                        return "\uf114"; // fa-folder-o
+                        break;
+                    case TreeType.Commit:
+                        return "\uf003"; //  fa-envelope-o
+                        break;
+                    default:
+                        return "\uf10c"; // fa-circle-o
+                }
+            }
+        }
 
         public string Path { get; set; }
         public string Mode { get; set; }
